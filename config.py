f6c214d822b8a12c63ffe7902491793ef8421f65
vladi2007 <ikonev05@mail.ru>
2024-10-28 14:36:34 +0500

    First commit

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+import config
+
+# параметры окружности
+R_min =  0
+R_max = 75
+w_min = 1 # мин.ширина обводки
+k_w_max = 0.2  # макс. ширина обводки k_w_max * R
+
+# параметры изображения
+size_x = 2560
+size_y = 1440
+N_circles_min = 1520  # мин. число окружностей-капель на картинку
+N_circles_max = 1521  # макс. число окружностей-капель на картинку
+
+# параметры датасета
+# пути сохраения входных и целевых изображений
+path_to_cirled_image = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + 'targets' + os.path.sep
+path_to_target_image = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + 'targets' + os.path.sep
+path_to_cirled_image_with_mask=os.path.dirname(os.path.abspath(__file__)) + os.path.sep + 'targets_with_mask' + os.path.sep
+path_to_mask=os.path.dirname(os.path.abspath(__file__)) + os.path.sep + 'result_masks' + os.path.sep
+N_images = 10 # общее количество изображений в датасете
---
+++ contur_mask.py
@@ -0,0 +1,17 @@
+import numpy as np
+from decimal import Decimal, getcontext
+#(640,80,1920,1360)
+file="./contur_mask/mask.txt"
+contur_mask_coords=[]
+def parse_contur_coords(path,result_list):
+    context=getcontext()
+    context.prec=30
+    with open(path , 'r', encoding='utf-8') as file:
+        for line in file:
+            coords_list=line.split()
+            for i in range(1,len(coords_list)-1,2):
+                x=Decimal(coords_list[i])*2560
+                y=Decimal(coords_list[i+1])*1440
+                if (640+20<x<1920-15) and (80+12<y<1360-2): #бывает что все равно вылезает за границы картинки - плюсую epsilon 
+                    result_list.append((x,y))
+parse_contur_coords(file,contur_mask_coords)                       
