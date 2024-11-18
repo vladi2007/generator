d6457a43550b629084f71572937b5da9e3236443
vladi2007 <ikonev05@mail.ru>
2024-11-18 23:26:22 +0500

    fix exception  with empty list of mask of drops, add comments, changing the return data  type

--- methods.py
+++ methods.py
@@ -4,7 +4,7 @@ from shapely.geometry import Polygon
 
 
 # завернул все в один метод
-def main(model_path,image_path,conf=0.2,iou=0,imgsz=640):
+def main(image_path,model_path='best.pt',conf=0.2,iou=0,imgsz=640):
 
 
     # загружаем модель
@@ -24,19 +24,24 @@ def main(model_path,image_path,conf=0.2,iou=0,imgsz=640):
     masks = result[0].masks.xy
 
 
-    # получаем список с вычислениями для каждой капли вида [(маска1, площадь1,центр1), (маска2,площадь2,центр2)...]
+    # получаем список с вычислениями для каждой капли вида [(маска1, площадь1, центр1), 
+    #                                                       (маска2, площадь2, центр2),
+    #                                                       (маска3, площадь3, центр3)
+    #                                                       ]
     return make_result_list(masks)
     
 
-# считает маску одной капли, берет mask и возвращает список кортежей вида (x1,x2)
+# вовзращает маску ОДНОЙ капли в ОТДЕЛЬНОСТИ
+# маска капли это список пикселей, один пиксель это кортеж с координатами этого пикселя вида (x1,y1)
+# точность координат до десятых, то есть каждая координата имеет тип float 
 def get_image_masks(mask):
     mask_of_water=[]
     for coords in mask:
-        mask_of_water.append((int(coords[0]),int(coords[1])))  
+        mask_of_water.append((float(coords[0]),float(coords[1])))  
     return mask_of_water
 
 
-# считаем площадь отдельной капли в пикселях
+# считаем площадь отдельной капли в ПИКСЕЛЯХ вернет float с точностью до десятых
 def count_object_area(mask):
     # Создаем объект Polygon из списка координат
     polygon = Polygon(mask)
@@ -44,7 +49,7 @@ def count_object_area(mask):
     return polygon.area
 
 
-# считаем центр капли получаем кортеж (x,y) там точность знаков 15, поэтому аккуратней
+# считаем центр капли получаем кортеж (x,y) вернет float  точность 12 знаков после запятой
 def find_object_center(mask):
     # Создаем объект Polygon из списка координат
     polygon = Polygon(mask)
@@ -55,12 +60,21 @@ def find_object_center(mask):
 
 # собираем кортеж для капли вида (маска, площадь, центр)
 def count_all_parameters_of_object_on_image(mask):
-    return (get_image_masks(mask),count_object_area(mask),find_object_center(mask))
+    image_mask=get_image_masks(mask)
+    # делаем проверку что в маска капли это не пустой список
+    if len(image_mask)>0:
+        return (get_image_masks(mask),count_object_area(mask),find_object_center(mask))
+    return None    
 
 
 # собираем итоговый список всех капель
 def make_result_list(masks):
     res_list=[]
     for mask in masks:
-        res_list.append(count_all_parameters_of_object_on_image(mask))
+        res_tuple=count_all_parameters_of_object_on_image(mask)
+        # если прошло проверку что в маска капли это не пустой список, то делаем вычисления дальше и добавляем
+        if res_tuple!=None:
+            res_list.append(count_all_parameters_of_object_on_image(mask))
     return  res_list
+
+print(main('aXkoF-teckw.jpg')[0])
