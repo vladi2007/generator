79d160bf0423515227cb63e8be09f5e8ac69fcea
vladi2007 <ikonev05@mail.ru>
2024-11-21 19:20:27 +0500

    refactoring,delete bad methods, fix bag with method Polygon

--- methods.py
+++ methods.py
@@ -1,10 +1,12 @@
 from ultralytics import YOLO
 from PIL import Image, ImageDraw
 from shapely.geometry import Polygon
+import os
+import numpy as np
 
 
 # завернул все в один метод
-def main(image_path,model_path='best.pt',conf=0.2,iou=0,imgsz=640):
+def main(image_path,model_path='best (4).pt',conf=0.5,iou=0.5,imgsz=640):
 
 
     # загружаем модель
@@ -17,53 +19,36 @@ def main(image_path,model_path='best.pt',conf=0.2,iou=0,imgsz=640):
 
     # загружаем картинку в модель, imgsz = 640, еще есть параметр conf - тип float от 0 до 1 включительно, 
     # это уверенность модели что обьект соотвествует и его надо выделять
-    result=model(image,imgsz=imgsz,conf=conf,iou=iou)
+    result=model(image,imgsz=imgsz,conf=conf,iou=iou,max_det = 2500,retina_masks=True)
 
 
-    # берем маски из обьекта result, xy - значит что координаты не нормамированные 
+    # # берем маски из обьекта result, xy - значит что координаты не нормамированные 
     masks = result[0].masks.xy
-
-
-    # получаем список с вычислениями для каждой капли вида [(маска1, площадь1, центр1), 
-    #                                                       (маска2, площадь2, центр2),
-    #                                                       (маска3, площадь3, центр3)
-    #                                                       ]
-    return make_result_list(masks)
     
 
-# вовзращает маску ОДНОЙ капли в ОТДЕЛЬНОСТИ
-# маска капли это список пикселей, один пиксель это кортеж с координатами этого пикселя вида (x1,y1)
-# точность координат до десятых, то есть каждая координата имеет тип float 
-def get_image_masks(mask):
-    mask_of_water=[]
-    for coords in mask:
-        mask_of_water.append((float(coords[0]),float(coords[1])))  
-    return mask_of_water
-
-
-# считаем площадь отдельной капли в ПИКСЕЛЯХ вернет float с точностью до десятых
-def count_object_area(mask):
-    # Создаем объект Polygon из списка координат
-    polygon = Polygon(mask)
-    # Возвращаем площадь многоугольника в пикселях
-    return polygon.area
-
-
-# считаем центр капли получаем кортеж (x,y) вернет float  точность 12 знаков после запятой
-def find_object_center(mask):
-    # Создаем объект Polygon из списка координат
-    polygon = Polygon(mask)
-    # Получаем центр многоугольника
-    centroid = polygon.centroid
-    return (centroid.x, centroid.y)  # Возвращаем координаты центра
-
+    result_list=make_result_list(masks)
+    # # # получаем список с вычислениями для каждой капли вида (count, [(маска1, площадь1, центр1), 
+    # # #                                                       (маска2, площадь2, центр2),
+    # # #                                                       (маска3, площадь3, центр3)
+    # # #    
+    # # #                                                    ])
+    return (len(result_list_),result_list)
+    
 
 # собираем кортеж для капли вида (маска, площадь, центр)
 def count_all_parameters_of_object_on_image(mask):
-    image_mask=get_image_masks(mask)
     # делаем проверку что в маска капли это не пустой список
-    if len(image_mask)>0:
-        return (get_image_masks(mask),count_object_area(mask),find_object_center(mask))
+    if len(mask)>0:
+        # замыкаем контур первым пикселем, чтобы полигон норм посчитало
+        new_mask = np.vstack([mask, mask[0]])
+        # считаем полигон собнсно
+        polygon=Polygon(new_mask) 
+        # берем площадь
+        area=polygon.area
+        # берем центр
+        center=polygon.centroid
+        # возвращаем итоговый кортеж
+        return (new_mask,area,(center.x,center.y))
     return None    
 
 
@@ -74,7 +59,5 @@ def make_result_list(masks):
         res_tuple=count_all_parameters_of_object_on_image(mask)
         # если прошло проверку что в маска капли это не пустой список, то делаем вычисления дальше и добавляем
         if res_tuple!=None:
-            res_list.append(count_all_parameters_of_object_on_image(mask))
+            res_list.append(res_tuple)
     return  res_list
-
-print(main('aXkoF-teckw.jpg')[0])
